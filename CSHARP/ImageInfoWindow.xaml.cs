b330f0ecb404ebb16c845588917e6f92f869d582
Sebastian Rutofski <sebrut@web.de>
2013-03-29 10:43:47 +0100

    work at uplaod method

--- CSHARP/ImageInfoWindow.xaml.cs
+++ CSHARP/ImageInfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -28,18 +29,17 @@ namespace dieBug
             this.imagePath = imagePath;
             image1.Source = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
         }
-        
+
         private void TransmitPicture()
         {
-            HttpWebRequest request = (HttpWebRequest) WebRequest.Create("http://lol.de/upload.php");
-            request.Method = WebRequestMethods.Http.Post;
-            request.ContentType = "multipart/form-data";
-            request.KeepAlive = true;
-            throw new NotImplementedException();
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("desc", "");
+            nvc.Add("btn-submit-photo", "Upload");
+            HttpUploadFile("http://your.server.com/upload", imagePath, "file", "image/bmp", nvc);
         }
 
         private void textBox_DeliverFrom_PreviewKeyDown(object sender, KeyEventArgs e)
-        {   
+        {
             if (e.Key == Key.Enter)
             {
                 if (descriptionbox.LineCount > 5)
@@ -69,7 +69,7 @@ namespace dieBug
         {
             var uriSource = new Uri(@"/dieBug;component/Images/f2_new_hover.png", UriKind.Relative);
             newphoto.Source = new BitmapImage(uriSource);
-            this.Close();        
+            this.Close();
         }
 
         // --WOOOOOOOH EPIC UPLOAD_BUTTON
@@ -126,6 +126,68 @@ namespace dieBug
             Environment.Exit(0);
         }
 
+        public static string HttpUploadFile(string url, string file, string paramName, string contentType, NameValueCollection nvc)
+        {
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
 
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            try
+            {
+                wresp = wr.GetResponse();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                return reader2.ReadToEnd();
+                reader2.Dispose();
+            }
+            catch (Exception ex)
+            {
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
+        }
     }
 }
