f0fc463858f7ac9ed6a63c2c9e75f3592c88017d
Sebastian Rutofski <sebrut@web.de>
2013-04-10 17:41:06 +0200

    Refactoring of ImageWindow and mainWindow

--- CSHARP/ImageInfoWindow.xaml.cs
+++ CSHARP/ImageInfoWindow.xaml.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Net;
-using System.IO;
-using System.Drawing;
+using System.Windows.Threading;
 using Microsoft.Win32;
-using System.Web;
 
 namespace dieBug
 {
     /// <summary>
-    /// Interaktionslogik für ImageInfoWindow.xaml
+    ///     Interaktionslogik für ImageInfoWindow.xaml
     /// </summary>
-    public partial class ImageInfoWindow : Window
+    public partial class ImageInfoWindow
     {
-        private string imagePath = String.Empty;
+        private readonly string _ImagePath = String.Empty;
+
+        private string _Description = String.Empty;
+        private bool _Isbiggered;
+        private string _UploadPassword;
+        private string _UploadPath;
+
         public ImageInfoWindow(string imagePath)
         {
             InitializeComponent();
-            this.imagePath = imagePath;
+            FileInfo fi = new FileInfo(imagePath);
+            this.Title = fi.Name;
+
+            _ImagePath = imagePath;
             try
             {
-                image1.Source = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+                ScreenshotImage.Source = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
             }
             catch (FileNotFoundException e)
             {
@@ -44,23 +46,19 @@ namespace dieBug
                 MessageBox.Show(e.Message);
             }
 
-            RegistryKey diebugKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\dieBug", RegistryKeyPermissionCheck.ReadSubTree);
-            UploadPath = (string)diebugKey.GetValue("UploadPath", String.Empty);
-            UploadPassword = (string)diebugKey.GetValue("UploadPassword", String.Empty);
+            RegistryKey diebugKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\dieBug",
+                                                                      RegistryKeyPermissionCheck.ReadSubTree);
+            _UploadPath = (string) diebugKey.GetValue("UploadPath", String.Empty);
+            _UploadPassword = (string) diebugKey.GetValue("UploadPassword", String.Empty);
 
             diebugKey.Close();
         }
 
-        private string UploadPassword;
-        private string UploadPath;
-        private string description = String.Empty;
-        private bool isbiggered = false;
-
         private void TransmitPicture()
         {
-            Dictionary<string, string> nvc = new Dictionary<string, string>();
+            var nvc = new Dictionary<string, string>();
 
-            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Input, new Action(delegate()
+            Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(delegate
                 {
                     descriptionbox.IsReadOnly = true;
                     upload.Visibility = Visibility.Hidden;
@@ -69,18 +67,107 @@ namespace dieBug
                     progressbarimg.Visibility = Visibility.Visible;
                     progressbarbar.Visibility = Visibility.Visible;
                 }));
-            nvc.Add("description", description);
-            string response = HttpUploadFile((string)UploadPath, imagePath, "datei", "image/png", nvc);
-            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Input, new Action(delegate()
+            nvc.Add("description", _Description);
+            string response = HttpUploadFile(_UploadPath, _ImagePath, "datei", "image/png", nvc);
+            Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(delegate
+                {
+                    progressbarimg.Visibility = Visibility.Hidden;
+                    progressbarbar.Visibility = Visibility.Hidden;
+                    urlimage.Visibility = Visibility.Visible;
+                    urlbox.Visibility = Visibility.Visible;
+                    urlbox.Text = response;
+                    urlbox.Focus();
+                    urlbox.SelectAll();
+                }));
+        }
+
+        private string HttpUploadFile(string url, string file, string paramName, string contentType,
+                                      Dictionary<string, string> nvc)
+        {
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = Encoding.UTF8.GetBytes("\r\n--" + boundary + "\r\n");
+
+            var wr = (HttpWebRequest) WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+            SetProgressbarToPercentage
+                (10);
+
+
+            int percentperkey = 40/nvc.Keys.Count;
+            double value = 10;
+
+            const string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
             {
-                progressbarimg.Visibility = Visibility.Hidden;
-                progressbarbar.Visibility = Visibility.Hidden;
-                urlimage.Visibility = Visibility.Visible;
-                urlbox.Visibility = Visibility.Visible;
-                urlbox.Text = response; 
-                urlbox.Focus();
-                urlbox.SelectAll();
-            }));
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+                SetProgressbarToPercentage(value + percentperkey);
+                value += percentperkey;
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            const string headerTemplate =
+                "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            var buffer = new byte[4096];
+            int bytesRead = 0;
+
+            int parts = (int) fileStream.Length/4096;
+            double percentperpart = 40.0/parts;
+            value = 50;
+
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+
+                SetProgressbarToPercentage(value + percentperpart);
+                value += percentperpart;
+            }
+            fileStream.Close();
+
+            byte[] trailer = Encoding.UTF8.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            string response = String.Empty;
+            try
+            {
+                wresp = wr.GetResponse();
+                SetProgressbarToPercentage
+                    (95);
+                Stream stream2 = wresp.GetResponseStream();
+                if (stream2 != null)
+                {
+                    var reader2 = new StreamReader(stream2);
+                    response = reader2.ReadToEnd();
+                    reader2.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    response = "Error";
+                }
+            }
+            finally
+            {
+                SetProgressbarToPercentage(100);
+            }
+            return response;
         }
 
         private void textBox_DeliverFrom_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -115,7 +202,7 @@ namespace dieBug
             var uriSource = new Uri(@"/dieBug;component/Images/f2_new_hover.png", UriKind.Relative);
             newphoto.Source = new BitmapImage(uriSource);
 
-            this.Close();
+            Close();
         }
 
         // --WOOOOOOOH EPIC UPLOAD_BUTTON
@@ -143,25 +230,27 @@ namespace dieBug
             var uriSource = new Uri(@"/dieBug;component/Images/f2_upload_hover.png", UriKind.Relative);
             upload.Source = new BitmapImage(uriSource);
 
-            if (UploadPath.Equals(String.Empty))
+            if (_UploadPath.Equals(String.Empty))
             {
-
-                RegistryKey diebugKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\dieBug", RegistryKeyPermissionCheck.ReadSubTree);
-                UploadPath = (string)diebugKey.GetValue("UploadPath", String.Empty);
-                UploadPassword = (string)diebugKey.GetValue("UploadPassword", String.Empty);
+                RegistryKey diebugKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\dieBug",
+                                                                          RegistryKeyPermissionCheck.ReadSubTree);
+                _UploadPath = (string) diebugKey.GetValue("UploadPath", String.Empty);
+                _UploadPassword = (string) diebugKey.GetValue("UploadPassword", String.Empty);
 
                 diebugKey.Close();
 
-                if (MessageBoxResult.Yes == MessageBox.Show("Es ist kein Uploadpfad eingetragen.\nMöchten sie die Einstellungen öffnen?", "Es ist ein Fehler aufgetreten.", MessageBoxButton.YesNo))
+                if (MessageBoxResult.Yes ==
+                    MessageBox.Show("Es ist kein Uploadpfad eingetragen.\nMöchten sie die Einstellungen öffnen?",
+                                    "Es ist ein Fehler aufgetreten.", MessageBoxButton.YesNo))
                 {
-                    SettingsWindow sw = new SettingsWindow();
+                    var sw = new SettingsWindow();
                     sw.Show();
                 }
             }
             else
             {
-                description = descriptionbox.Text;
-                Thread t = new Thread(TransmitPicture);
+                _Description = descriptionbox.Text;
+                var t = new Thread(TransmitPicture);
                 t.Start();
             }
         }
@@ -182,7 +271,6 @@ namespace dieBug
 
         private void delete_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
             var uriSource = new Uri(@"/dieBug;component/Images/f2_delete_active.png", UriKind.Relative);
             delete.Source = new BitmapImage(uriSource);
         }
@@ -194,126 +282,24 @@ namespace dieBug
             Environment.Exit(0);
         }
 
-        public string HttpUploadFile(string url, string file, string paramName, string contentType, Dictionary<string, string> nvc)
-        {
-            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
-            byte[] boundarybytes = System.Text.Encoding.UTF8.GetBytes("\r\n--" + boundary + "\r\n");
-
-            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
-            wr.ContentType = "multipart/form-data; boundary=" + boundary;
-            wr.Method = "POST";
-            wr.KeepAlive = true;
-            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
-
-            Stream rs = wr.GetRequestStream();
-            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(delegate()
-            {
-                SetProgressbarToPercentage(10);
-            }));
-
-
-            int percentperkey = 40/nvc.Keys.Count;
-            double value = 10;
-
-            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
-            foreach (string key in nvc.Keys)
-            {
-                rs.Write(boundarybytes, 0, boundarybytes.Length);
-                string formitem = string.Format(formdataTemplate, key, nvc[key]);
-                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
-                rs.Write(formitembytes, 0, formitembytes.Length);
-                SetProgressbarToPercentage(value + percentperkey);
-                value += percentperkey;
-            }
-            rs.Write(boundarybytes, 0, boundarybytes.Length);
-
-            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
-            string header = string.Format(headerTemplate, paramName, file, contentType);
-            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
-            rs.Write(headerbytes, 0, headerbytes.Length);
-
-            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-            byte[] buffer = new byte[4096];
-            int bytesRead = 0;
-
-            int parts = (int)fileStream.Length/4096;
-            double percentperpart = 40.0 / parts;
-            value = 50;
-
-            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
-            {
-                rs.Write(buffer, 0, bytesRead);
-
-                SetProgressbarToPercentage(value + percentperpart);
-                value += percentperpart;
-            }
-            fileStream.Close();
-            
-            byte[] trailer = System.Text.Encoding.UTF8.GetBytes("\r\n--" + boundary + "--\r\n");
-            rs.Write(trailer, 0, trailer.Length);
-            rs.Close();
-
-            WebResponse wresp = null;
-            string response = String.Empty;
-            try
-            {
-                wresp = wr.GetResponse();
-                this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(delegate()
-                {
-                    SetProgressbarToPercentage(95);
-                }));
-                Stream stream2 = wresp.GetResponseStream();
-                StreamReader reader2 = new StreamReader(stream2);
-                response = reader2.ReadToEnd();
-                reader2.Dispose();
-            }
-            catch (Exception ex)
-            {
-                if (wresp != null)
-                {
-                    wresp.Close();
-                    wresp = null;
-                    response = "Error";
-                }
-            }
-            finally
-            {
-                wr = null;
-                this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate()
-                {
-                    SetProgressbarToPercentage(100);
-                }));
-            }
-            return response;
-        }
-
         private void SetProgressbarToPercentage(double value)
         {
-            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(delegate()
-            {
-                progressbarbar.Width = 384 / 100 * value;
-            }));
+            Dispatcher.BeginInvoke(DispatcherPriority.Background,
+                                   new Action(delegate { progressbarbar.Width = 384/100*value; }));
         }
 
-        private void image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void ScreenshotImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (!isbiggered)
+            if (!_Isbiggered)
             {
-                image1.Width *= 1.5;
-                isbiggered = true;
+                ScreenshotImage.Width *= 1.5;
+                _Isbiggered = true;
             }
             else
             {
-                image1.Width *= 0.75;
-                isbiggered = false;
+                ScreenshotImage.Width *= 0.75;
+                _Isbiggered = false;
             }
-        }
-
-
-        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            MainWindow mw = new MainWindow();
-            mw.Show();
         }
 
         private void f1_close_MouseEnter(object sender, MouseEventArgs e)
@@ -339,6 +325,12 @@ namespace dieBug
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_close_hover.png", UriKind.Relative);
             f1_close.Source = new BitmapImage(uriSource);
             Environment.Exit(0);
+        }
+
+        private void Window_Closing_1(object sender, CancelEventArgs e)
+        {
+            var mw = new MainWindow();
+            mw.Show();
         }
     }
 }
--- CSHARP/MainWindow.xaml.cs
+++ CSHARP/MainWindow.xaml.cs
@@ -1,148 +1,139 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Media.Animation;
-using System.IO;
-using Path = System.IO.Path;
 using System.Drawing.Imaging;
-using Microsoft.Win32;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace dieBug
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    ///     Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly string _BaseDir;
+        private readonly string _ShotDir;
+
         public MainWindow()
         {
-            baseDir = Environment.GetFolderPath((Environment.SpecialFolder.ApplicationData)) + Path.DirectorySeparatorChar + "dieBug";
-            shotDir = baseDir + Path.DirectorySeparatorChar + "shots";
+            _BaseDir = Environment.GetFolderPath((Environment.SpecialFolder.ApplicationData)) +
+                       Path.DirectorySeparatorChar + "dieBug";
+            _ShotDir = _BaseDir + Path.DirectorySeparatorChar + "shots";
             InitializeComponent();
             InitFileSystem();
         }
-        private string baseDir;
-        private string shotDir;
 
         private void InitFileSystem()
         {
-            if (!Directory.Exists(baseDir))
-                Directory.CreateDirectory(baseDir);
-            if (!Directory.Exists(shotDir))
-                Directory.CreateDirectory(shotDir);
+            if (!Directory.Exists(_BaseDir))
+                Directory.CreateDirectory(_BaseDir);
+            if (!Directory.Exists(_ShotDir))
+                Directory.CreateDirectory(_ShotDir);
+        }
+
+        private void OpenAbout()
+        {
+            var aw = new AboutWindow();
+            aw.Show();
         }
 
         private void OpenImageInfo(string imagePath)
         {
-            ImageInfoWindow iiw = new ImageInfoWindow(imagePath);
-            this.Visibility = Visibility.Hidden;
+            var iiw = new ImageInfoWindow(imagePath);
+            Visibility = Visibility.Hidden;
             iiw.Show();
         }
 
-        private void f1_shoot_MouseDown(object sender, MouseButtonEventArgs e)
+        private void OpenSettings()
         {
-            var uriSource = new Uri(@"/dieBug;component/Images/f1_button_shoot_active.png", UriKind.Relative);
-            f1_shoot.Source = new BitmapImage(uriSource);
-        }
-
-        private void f1_shoot_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            var uriSource = new Uri(@"/dieBug;component/Images/f1_button_shoot_normal.png", UriKind.Relative);
-            f1_shoot.Source = new BitmapImage(uriSource);
-            this.WindowState = WindowState.Minimized;
-            Bitmap screenShot = ScreenCapture.Screen();
-            this.WindowState = WindowState.Normal;
-            string filename = DateTime.Now.ToShortDateString().Replace(".", "-") + "-" + DateTime.Now.ToShortTimeString().Replace(":", "-") + "-" + DateTime.Now.Second.ToString() + ".png";
-            string path = Path.Combine(shotDir, filename);
-            screenShot.Save(path, ImageFormat.Png);
-            OpenImageInfo(path);
-            this.Close();
-        }
-
-        private void f1_background_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
+            var sw = new SettingsWindow();
+            sw.Show();
         }
 
         private void Window_MouseLeave(object sender, MouseEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/f1_button_shoot_normal.png", UriKind.Relative);
-            f1_shoot.Source = new BitmapImage(uriSource);
+            F1Shoot.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_close_MouseEnter(object sender, MouseEventArgs e)
+        private void F1Shoot_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var uriSource = new Uri(@"/dieBug;component/Images/f1_button_shoot_active.png", UriKind.Relative);
+            F1Shoot.Source = new BitmapImage(uriSource);
+        }
+
+        private void F1Shoot_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var uriSource = new Uri(@"/dieBug;component/Images/f1_button_shoot_normal.png", UriKind.Relative);
+            F1Shoot.Source = new BitmapImage(uriSource);
+            WindowState = WindowState.Minimized;
+            Bitmap screenShot = ScreenCapture.Screen();
+            WindowState = WindowState.Normal;
+            string filename = DateTime.Now.ToShortDateString().Replace(".", "-") + "-" +
+                              DateTime.Now.ToShortTimeString().Replace(":", "-") + "-" +
+                              DateTime.Now.Second.ToString(CultureInfo.InvariantCulture) + ".png";
+            string path = Path.Combine(_ShotDir, filename);
+            screenShot.Save(path, ImageFormat.Png);
+            OpenImageInfo(path);
+            Close();
+        }
+
+        private void f1_background_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void F1Close_MouseEnter(object sender, MouseEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_close_hover.png", UriKind.Relative);
-            f1_close.Source = new BitmapImage(uriSource);
+            F1Close.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_close_MouseLeave(object sender, MouseEventArgs e)
+        private void F1Close_MouseLeave(object sender, MouseEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_close_normal.png", UriKind.Relative);
-            f1_close.Source = new BitmapImage(uriSource);
+            F1Close.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_close_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void F1Close_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_close_active.png", UriKind.Relative);
-            f1_close.Source = new BitmapImage(uriSource);
+            F1Close.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_close_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void F1Close_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_close_hover.png", UriKind.Relative);
-            f1_close.Source = new BitmapImage(uriSource);
+            F1Close.Source = new BitmapImage(uriSource);
             Environment.Exit(0);
         }
 
-        private void f1_settings_MouseEnter(object sender, MouseEventArgs e)
+        private void F1Settings_MouseEnter(object sender, MouseEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_options_hover.png", UriKind.Relative);
-            f1_settings.Source = new BitmapImage(uriSource);
+            F1Settings.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_settings_MouseLeave(object sender, MouseEventArgs e)
+        private void F1Settings_MouseLeave(object sender, MouseEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_options_normal.png", UriKind.Relative);
-            f1_settings.Source = new BitmapImage(uriSource);
+            F1Settings.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_settings_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void F1Settings_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_options_active.png", UriKind.Relative);
-            f1_settings.Source = new BitmapImage(uriSource);
+            F1Settings.Source = new BitmapImage(uriSource);
         }
 
-        private void f1_settings_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void F1Settings_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             var uriSource = new Uri(@"/dieBug;component/Images/fall_button_options_hover.png", UriKind.Relative);
-            f1_settings.Source = new BitmapImage(uriSource);
+            F1Settings.Source = new BitmapImage(uriSource);
             OpenSettings();
-        }
-
-        private void OpenSettings()
-        {
-            SettingsWindow sw = new SettingsWindow();
-            sw.Show();
-        }
-
-        private void OpenAbout()
-        {
-            AboutWindow aw = new AboutWindow();
-            aw.Show();
         }
     }
 }
